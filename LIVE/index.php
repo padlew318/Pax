fdc1bd7c2387942d1848ba7c5134e23014a87102
Paddy Lewis <paddy.lewis@channelcentral.net>
2022-07-31 13:47:49 +0100

    added live

---
+++ LIVE/index.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<meta name="description" content="Pax Place - Holiday cottage to rent in Portloe Cornwall, cottage to let in portloe cornwall" />
+<meta name="keywords" content="holiday cottage to let, holiday cottage to rent, portloe, cornwall, cottage in portloe, holiday cottage in portloe, roseland, holiday accomodation" />
+<meta name="robots" content="index,follow" />
+<meta name="author" content="paddy@pagancomputers.co.uk" />
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+<title>Pax Place - Holiday Cottage Portloe Cornwall</title>
+<script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-7646257-11']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
+<style>
+body, html {
+    height: 100%;
+    margin: 0;
+    font-family: arial;
+}
+#header{
+	background: #113251;
+	width:100%;
+	padding: 12px;
+	text-align: center;
+}
+#header h1{
+	color: white;
+	margin: 0px;
+}
+
+.bg {
+    /* The image used */
+    background-image: url("IMGP7135.jpg");
+
+    /* Full height */
+    height: 100%; 
+
+    /* Center and scale the image nicely */
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+	
+	position:relative;
+
+}
+.holding{
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  -webkit-transform: translate(-50%, -50%);
+  -moz-transform: translate(-50%, -50%);
+  -o-transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+
+  opacity:1;
+  padding:30px;
+  border: 1px solid rgba(255,255,255, 0.75);
+  border-radius:10px;
+  text-align: center;
+
+  background:rgba(100,100,100, 0.75);
+
+  color:#fff;
+  text-shadow: 0px 1px #333;
+}
+.holding H1{
+  font-size: 40px;
+}
+.holding H2{
+  font-size: 32px;
+  letter-spacing: 8px;
+  text-transform: uppercase;
+}
+.holding H3{
+  font-size: 28px;
+  letter-spacing: 10px;
+  text-transform: uppercase;
+}
+.holding H4{
+  font-size: 24px;
+  letter-spacing: 12px;
+  text-transform: uppercase;
+}
+.holding p{
+  color:#fff;
+  text-shadow: 0px 1px #333;
+  font-size: 24px;
+}
+ a {
+   color: white;
+   font-weight: 700;
+ }
+ #footer{
+	background: #113251;
+	width:100%;
+	padding: 12px;
+	text-align: center; 
+	color: #fff;
+	position:absolute; 
+	bottom:0;
+ }
+</style>
+</head>
+<body>
+<div class="bg">
+<div id="header">
+  <h1>Pax Place Holiday Cottage, Portloe, Cornwall</h1>
+</div>
+
+
+  <div class="holding">
+  <h2>Wild Seas</h2>
+  <h3>Salty Lips</h3>
+  <h4>Sandy Toes</h4>
+  <h2>White Surf</h2>
+  <h3>Sea Spray</h3>
+  <h4>Hidden Coves</h4>
+  </div>
+
+
+
+<!-- Footer -->
+<div id="footer">
+<!-- Copyright -->
+<div id="copyright">
+    <div class="container">
+     <span>&copy; 2018 - J Kissel</span>
+    </div>
+</div>
+</div>
+</div>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-7646257-11']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
+</body>
+</html>
